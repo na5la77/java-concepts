20c2ce08bbcdc6a6d381bc6c54dc468ccc16671d
Shadi Nakhla <shadinakhla13@gmail.com>
2025-02-05 17:46:19 +0200

    Starting a new service

---
+++ content-calendar/src/main/java/com/javaconcepts/contentcalendar/ContentCalendarApplication.java
@@ -0,0 +1,18 @@
+package com.javaconcepts.contentcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class ContentCalendarApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(ContentCalendarApplication.class, args);
+		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+	}
+
+}
